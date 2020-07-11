70ef0cdd7ce229336adc8ff0bddc64ce9552d5db
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-07-11 23:05:24 +0200

    Updated to react/http 1.0 and PHP 7.4 + all new goodies

--- examples/basic.php
+++ examples/basic.php
@@ -1,23 +1,25 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use Psr\Http\Message\ServerRequestInterface;
 use React\EventLoop\Factory;
 use React\Http\Response;
 use React\Http\Server as HttpServer;
 use React\Socket\Server as SocketServer;
+use WyriHaximus\React\Http\Middleware\Rewrite;
 use WyriHaximus\React\Http\Middleware\RewriteMiddleware;
 
-require \dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php';
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-$loop = Factory::create();
+$loop   = Factory::create();
 $socket = new SocketServer('127.0.0.1:9001', $loop);
-$http = new HttpServer([
-    new RewriteMiddleware([
-        '/' => '/index.html',
-    ]),
-    function (ServerRequestInterface $request) {
+$http   = new HttpServer(
+    $loop,
+    new RewriteMiddleware(new Rewrite('/', '/index.html')),
+    static function (ServerRequestInterface $request) {
         return new Response(200);
     },
-]);
+);
 $http->listen($socket);
 $loop->run();
--- src/RewriteMiddleware.php
+++ src/RewriteMiddleware.php
@@ -1,24 +1,35 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace WyriHaximus\React\Http\Middleware;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use React\Promise\PromiseInterface;
+
+use function array_key_exists;
 
 final class RewriteMiddleware
 {
-    /** @var array */
-    private $rewrites;
+    /** @var array<string, string> */
+    private array $rewrites = [];
 
-    public function __construct(array $rewrites)
+    public function __construct(Rewrite ...$rewrites)
     {
-        $this->rewrites = $rewrites;
+        foreach ($rewrites as $rewrite) {
+            $this->rewrites[$rewrite->from()] = $rewrite->to();
+        }
     }
 
+    /**
+     * @return PromiseInterface|ResponseInterface
+     */
     public function __invoke(ServerRequestInterface $request, callable $next)
     {
         $uri = $request->getUri();
 
-        if (isset($this->rewrites[$uri->getPath()])) {
+        if (array_key_exists($uri->getPath(), $this->rewrites)) {
             $request = $request->withUri($uri->withPath($this->rewrites[$uri->getPath()]));
         }
 
